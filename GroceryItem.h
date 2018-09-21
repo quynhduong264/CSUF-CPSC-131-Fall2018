9c1e9d3e4465d36f0233998ff2cfecd895089ebc
quynhduong264 <35977101+quynhduong264@users.noreply.github.com>
2018-09-20 22:59:54 -0700

    Add files via upload

--- GroceryItem.h
+++ GroceryItem.h
@@ -3,17 +3,18 @@
 #include <string>
 using namespace std;
 
-class GroceryItem {
-private:
+class GroceryItem 
+{
+  private:
     string _name;
     int _quantity;
     float _unitPrice;
     bool _taxable;
 
-public:
-    GroceryItem();
-    GroceryItem(const string&, const int&, const float&, const bool&);
-
+  public:
+    GroceryItem(); //default constructor
+    GroceryItem(const string&, const int&, const float&, const bool&); //overloaded construtor
+    //accessor functions
     string getName() const;
     void setName(const string&);
     int getQuantity() const;
@@ -23,3 +24,37 @@ public:
     bool isTaxable() const;
     void setTaxable(const bool&);
 };
+
+GroceryItem::GroceryItem() //default constructor
+{
+  _name = " ";
+  _quantity = 0;
+  _unitPrice = 0;
+  _taxable = 0;
+}
+
+GroceryItem::GroceryItem(const string& name, const int& quan, const float& unit, const bool& tax) //overloaded constructure
+{
+  _name = name;
+  _quantity = quan;
+  _unitPrice = unit;
+  _taxable = tax;
+}
+
+//Accessor Functions
+string GroceryItem::getName() const
+{
+  return _name;
+}
+int GroceryItem::getQuantity() const
+{
+  return _quantity;
+}
+float GroceryItem::getUnitPrice() const
+{
+  return _unitPrice;
+}
+bool GroceryItem::isTaxable() const
+{
+  return _taxable;
+}
