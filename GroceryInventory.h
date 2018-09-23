7f86e0b619d0774597aa1f020be86e0fd587725b
quynhduong264 <35977101+quynhduong264@users.noreply.github.com>
2018-09-23 16:58:29 -0700

    Update GroceryInventory.h

--- GroceryInventory.h
+++ GroceryInventory.h
@@ -9,43 +9,43 @@ using namespace std;
 
 class GroceryInventory {
 private:
-    vector<GroceryItem> _inventory;
-    float _taxRate;
+    vector<GroceryItem> _inventory; //vector called _inventory
+    float _taxRate; 
 
 public:
-    GroceryInventory()
+    GroceryInventory() //default constructor
     {
       _taxRate = 0;
     }
 
-    GroceryItem& getEntry(const string& n)
+    GroceryItem& getEntry(const string& n) //This function get the item name from GroceryItem
     {
       for (int i = 0; i < _inventory.size(); i++)
       {
-        if (_inventory[i].getName() == n)
+        if (_inventory[i].getName() == n) //If name is valid, return name
         {
           return _inventory[i];
         }
       }
-      throw invalid_argument("Invalid Name");
+      throw invalid_argument("Invalid Name"); //If the name is invalid, print Invalid Name
     }
-    void addEntry(const string& n, const int& q, const float& u, const bool& t)
+    void addEntry(const string& n, const int& q, const float& u, const bool& t) //This function adding products into vector
     {
       GroceryItem g(n, q, u, t);
       _inventory.push_back(g);
     }
-    void setTaxRate(const float& r)
+    void setTaxRate(const float& r) // set the Tax Rate
     {
       _taxRate = r;
     }
-    float getTaxRate() const
+    float getTaxRate() const //get Tax Rate
     {
       return _taxRate;
     }
-    void createListFromFile(const string&);
-    float calculateUnitRevenue() const;//unit price x quantity price
-    float calculateTaxRevenue() const;//unit price x tax
-    float calculateTotalRevenue() const;//tax + unit revenue
+    void createListFromFile(const string&); 
+    float calculateUnitRevenue() const; //This function calculate unit price x quantity price
+    float calculateTaxRevenue() const; //This function calculate tax(7.75) x unit price x quantity price
+    float calculateTotalRevenue() const; //This function calculate tax revenue + unit revenue
 
     GroceryItem& operator[](const int&);
 };
@@ -68,37 +68,37 @@ void GroceryInventory::createListFromFile(const string& filename) {
     }
 }
 
-float GroceryInventory::calculateUnitRevenue() const
+float GroceryInventory::calculateUnitRevenue() const //This function calculate unit price x quantity price
 {
   float revenue;
   float temp;
-  for (int i = 0; i < _inventory.size(); i++)
+  for (int i = 0; i < _inventory.size(); i++) //loop through the _inventory vector
   {
-    temp = _inventory[i].getUnitPrice() * _inventory[i].getQuantity();
+    temp = _inventory[i].getUnitPrice() * _inventory[i].getQuantity(); //calcuate unit revenue
     revenue += temp;
   }
   return revenue; 
 }
 
-float GroceryInventory::calculateTaxRevenue() const
+float GroceryInventory::calculateTaxRevenue() const //This function calculate tax(7.75) x unit price x quantity price
 {
   float taxRevenue = 0;
   float temp = 0;
-  for (int i = 0; i < _inventory.size(); i++)
+  for (int i = 0; i < _inventory.size(); i++) //loop through the +inventory vector
   {
-    if (_inventory[i].isTaxable() == true)
+    if (_inventory[i].isTaxable() == true) //If it is taxable = true (1) or else = false (0)
     {
-      temp = (_taxRate / 100) * _inventory[i].getUnitPrice() * _inventory[i].getQuantity();
+      temp = (_taxRate / 100) * _inventory[i].getUnitPrice() * _inventory[i].getQuantity(); //calculate tax x unit x quantity
       taxRevenue += temp;
     }
   }
   return taxRevenue;
 }
 
-float GroceryInventory::calculateTotalRevenue() const
+float GroceryInventory::calculateTotalRevenue() const //This function calculate tax revenue + unit revenue
 {
   float total = 0;
-  total = calculateUnitRevenue() + calculateTaxRevenue();
+  total = calculateUnitRevenue() + calculateTaxRevenue(); //calcuate total
   return total;
 }
 
